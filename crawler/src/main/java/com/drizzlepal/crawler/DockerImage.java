f534750c7e0c1edc55942a308884575a191608fa
drizzlepal <songyuwong@163.com>
2025-02-28 10:36:20 +0800

    feat(crawler): 添加爬虫模块获取可用 Docker 镜像
    - 新增 crawler 模块，用于爬取最新的可用 Docker 镜像
    - 实现了从指定网站爬取镜像信息并输出更新命令的功能
    - 在根 pom.xml 中添加 crawler 子模块
    - 更新根 pom.xml 中的依赖管理，添加 jsoup 和 httpclient 依赖

---
+++ crawler/src/main/java/com/drizzlepal/crawler/DockerImage.java
@@ -0,0 +1,70 @@
+package com.drizzlepal.crawler;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringJoiner;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.select.Elements;
+
+/**
+ * 爬取最新的可用的Docker镜像
+ */
+public class DockerImage {
+
+    public static void main(String[] args) throws ClientProtocolException, IOException {
+        String html = "";
+        Set<String> images = new HashSet<>();
+        // 使用 httpclient 请求docker镜像分享获取html内容
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpGet httpGet = new HttpGet("https://www.coderjia.cn/archives/dba3f94c-a021-468a-8ac6-e840f85867ea");
+            // 执行请求
+            try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
+                // 获取响应的实体
+                HttpEntity entity = response.getEntity();
+                // 打印响应内容
+                html = EntityUtils.toString(entity, "UTF-8");
+                // 确保响应实体被正确释放
+                EntityUtils.consume(entity);
+            }
+        }
+        // 使用 jsoup 解析html内容
+        Jsoup.parse(html).select("#lightGallery > table > tbody > tr").forEach(tr -> {
+            Elements item = tr.select("td");
+            if ("正常".equals(item.get(1).text())) {
+                images.add("\"https://" + item.get(0).select("code").text() + "\"");
+            }
+        });
+        // 输出镜像内容
+
+        StringJoiner stringJoiner = new StringJoiner(",\n", """
+                sudo tee /etc/docker/daemon.json <<-'EOF'
+                {
+                    "registry-mirrors": [
+                        """, """
+
+                        ]
+                    }
+
+                EOF
+                                    """);
+        images.forEach(image -> {
+            stringJoiner.add("        " + image);
+        });
+        StringBuilder res = new StringBuilder("");
+        res.append("# docker 镜像更新请执行以下命令：\n");
+        res.append(stringJoiner.toString());
+        res.append("# 重启docker服务\n" + //
+                "sudo systemctl daemon-reload && sudo systemctl restart docker");
+        System.out.println(res.toString());
+    }
+
+}
