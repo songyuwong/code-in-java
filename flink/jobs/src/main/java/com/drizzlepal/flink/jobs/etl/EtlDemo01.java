c5e25cb03f7eff122fb558624a79c7122f953483
songyu <songyuwong@163.com>
2025-08-29 14:21:55 +0800

    refactor(project): 重构项目模块并优化代码
    - 移除 springboot2 和 springboot3 相关模块
    - 新增 flink 模块
    - 更新项目依赖管理
    - 重构 DatasourceTest 类，使用 ColumnMetaData 替代 TableMetaData
    - 删除未使用的 WebStarterConfig、TestApplication 等类

---
+++ flink/jobs/src/main/java/com/drizzlepal/flink/jobs/etl/EtlDemo01.java
@@ -0,0 +1,9 @@
+package com.drizzlepal.flink.jobs.etl;
+
+public class EtlDemo01 {
+
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+
+}
--- jdbc/src/test/java/com/drizzlepal/jdbc/DatasourceTest.java
+++ jdbc/src/test/java/com/drizzlepal/jdbc/DatasourceTest.java
@@ -1,14 +1,14 @@
 package com.drizzlepal.jdbc;
 
 import java.sql.SQLException;
-import java.util.List;
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
 
 import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONWriter.Feature;
 import com.drizzlepal.jdbc.exception.JdbcException;
-import com.drizzlepal.jdbc.metadata.TableMetaData;
+import com.drizzlepal.jdbc.metadata.ColumnMetaData;
 
 public class DatasourceTest {
 
@@ -16,13 +16,12 @@ public class DatasourceTest {
     public void testGetDatasource() throws JdbcException, SQLException {
         DataSource datasource = DataSourceBuilder.builder(DatabaseType.MYSQL).host("127.0.0.1").port(3306)
                 .userName("root").password("KJGBKAHJSKDAHBS@$@Qadf@$!@wf(*&").database("drptest").build();
-        List<String> defaultColumnsResultSetMetaDataColumnLabels = datasource
-                .getDefaultColumnsResultSetMetaDataColumnLabels("data_exchange_log");
-        System.out.println(defaultColumnsResultSetMetaDataColumnLabels);
-        List<String> tableNames = datasource.getTableNames();
-        System.out.println(tableNames);
-        TableMetaData tableMetaData = datasource.getTableMetaData("lab_management");
-        System.out.println(JSON.toJSONString(tableMetaData, Feature.PrettyFormat, Feature.WriteMapNullValue));
+        ArrayList<ColumnMetaData> columnMetaDataFormSql = datasource.getColumnMetaDataFormSql(
+                "SELECT current_tasks FROM drptest.lab_management;");
+        System.out.println(JSON.toJSONString(columnMetaDataFormSql, Feature.PrettyFormat));
+        ArrayList<ColumnMetaData> columnMetaDataFormSql2 = datasource.getColumnMetaDataFormSql(
+                "SELECT concat(current_tasks,'') as current_tasks FROM drptest.lab_management;");
+        System.out.println(JSON.toJSONString(columnMetaDataFormSql2, Feature.PrettyFormat));
     }
 
 }
